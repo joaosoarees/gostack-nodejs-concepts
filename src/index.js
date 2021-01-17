9e475ac173ab7b9332dddbb65c25ce7508060326
João Vitor <jsoareez@gmail.com>
2021-01-17 05:00:16 -0300

    test all main parameters

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.get('/projects', (request, response) => response.json([
-  'Projeto 1',
-  'Projeto 2',
-  'Projeto 3',
-]));
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log({ title });
+  console.log({ owner });
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log({ title });
+  console.log({ owner });
+
   response.json([
     'Projeto 1',
     'Projeto 2',
@@ -18,6 +32,10 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log({ id });
+
   response.json([
     'Projeto 5',
     'Projeto 2',
