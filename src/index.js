5e36be4bc34dd5e6138677e070880ff957e69c28
João Vitor <jsoareez@gmail.com>
2021-01-17 04:42:14 -0300

    create routes to test on insomnia

--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.get('/projects', (request, response) => response.json([
+  'Projeto 1',
+  'Projeto 2',
+  'Projeto 3',
+]));
+
+app.post('/projects', (request, response) => {
+  response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  response.json([
+    'Projeto 5',
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
 
 app.listen(3333, () => console.log('🔥 Server listen http://localhost:3333'));
