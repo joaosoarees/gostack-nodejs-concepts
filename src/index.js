312a7d20545de9d19d8744b8c898fe6d110ddf8b
João Vitor <jsoareez@gmail.com>
2021-01-17 05:20:21 -0300

    create functional http methods

--- src/index.js
+++ src/index.js
@@ -1,55 +1,57 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
+const projects = [];
 
-  console.log({ title });
-  console.log({ owner });
-
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
-});
+app.get('/projects', (request, response) => response.json(projects));
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;
 
-  console.log({ title });
-  console.log({ owner });
+  const project = { id: uuidv4(), title, owner };
 
-  response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4',
-  ]);
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log({ id });
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-  response.json([
-    'Projeto 5',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project not found.' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project not found.' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => console.log('🔥 Server listen http://localhost:3333'));
